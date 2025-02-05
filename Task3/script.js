5efe4fc37f5372e4d921d180e84ee4c8d3ef8cfa
Ayush <ayubag21@gmail.com>
2025-02-06 00:43:48 +0530

    task3 done

---
+++ Task3/script.js
@@ -0,0 +1,4 @@
+function openTab(event, tabId) {
+    document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
+    document.getElementById(tabId).classList.add('active');
+}
